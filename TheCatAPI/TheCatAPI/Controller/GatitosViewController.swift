cf3a99cb1671ff5a4e050066b7eae1a0c4fc57a5
John <john@Air-de-Veronica.Dlink>
2022-11-02 21:51:28 -0300

    Created TableView e Scroll View

--- TheCatAPI/TheCatAPI/Controller/GatitosViewController.swift
+++ TheCatAPI/TheCatAPI/Controller/GatitosViewController.swift
@@ -10,34 +10,97 @@ import UIKit
 class GatitosViewController: UIViewController {
     
     // MARK: - Properties
-    
-    var gatitosDetailsView = GatitosDetailsVIew()
+    // translateMask setado com falso Permite ajustar constraints manualmente
+    private let tableView = UITableView(translateMask: false).apply {
+        $0.separatorStyle = .none // Seperador de Linhas, neste caso, náo temos
+        $0.backgroundColor = .clear
+        $0.allowsSelection = true // Permite que a célula seja selecionada
+        $0.bounces = true // Permite um efeito suave no scroll da lista
+        $0.showsVerticalScrollIndicator = false // Oculta a barra de rolagem ao scrollar
+    }
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 }
 
+// MARK: - CodeView
 extension GatitosViewController: CodeView {
     func buildViewHierarchy() {
-        view.addSubview(gatitosDetailsView)
+        view.addSubview(tableView)
     }
     
     func setupConstraints() {
-        
-        gatitosDetailsView.anchor(
+        tableView.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
             left: view.leftAnchor,
             bottom: view.bottomAnchor,
-            right: view.rightAnchor
+            right: view.rightAnchor,
+            paddingLeft: 16,
+            paddingRight: 16
         )
     }
     
     func setupAdditionalConfiguration() {
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        title = "Raças"
+        tableView.register(GatitosViewCell.self, forCellReuseIdentifier: "GatitosViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension GatitosViewController: UITableViewDelegate, UITableViewDataSource {
+    // Quantidade de seções
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    // Numero de linhas dentro da Seção
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
     
+    // Altura da célula
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    // Costumiza a tableView como um todo
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+        // Este trecho de código faz com que cada célula tenha um espaçamento entre elas
+        // ---------------------------------------------------------------------------------
+        let verticalPadding: CGFloat = 8
+        let maskLayer = CALayer()
+        maskLayer.cornerRadius = 4 // Bordar arredondadas
+        maskLayer.backgroundColor = UIColor.black.cgColor
+        maskLayer.frame = CGRect(
+            x: cell.bounds.origin.x,
+            y: cell.bounds.origin.y,
+            width: cell.bounds.width,
+            height: cell.bounds.height
+        ).insetBy(
+            dx: 0,
+            dy: verticalPadding/2
+        )
+        cell.layer.mask = maskLayer
+        // ---------------------------------------------------------------------------------
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GatitosViewCell", for: indexPath) as! GatitosViewCell
+        cell.label.text = "John"
+        return cell
+    }
+    
+    // Configura a função do clique das células.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = GatitosDetailsViewController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
 }
 
---
+++ TheCatAPI/TheCatAPI/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  TheCatAPI
+//
+//  Created by John on 02/11/22.
+//
+
+import Foundation
+
+public class Constants {
+    
+    public let DESCRIPTION = "Lorem ipsum dolor sit amet. Qui mollitia esse et molestiae earum qui molestiae nobis. Et quis libero vel maiores magni ut nisi neque. Ab quia molestiae id reiciendis galisum qui corporis officia est incidunt porro. Sit delectus repellat ex explicabo harum in possimus recusandae et labore quia."
+    
+    public let TEMPERAMENT = "Agitado Carinhoso Calmo Calmo Agitado Carinhoso Raivoso Calmo Agitado Calmo dócil Agitado dócil Carinhoso. Carinhoso Agitado Raivoso Calmo Calmo Raivoso."
+}
+
+
